9e2caba766277a5792251c4fe6ff7df484a1abeb
Duron-Cleaning <duroncleaningservicesllc@gmail.com>
2025-01-15 08:14:22 -0600

    Add files via upload

---
+++ move-icons.ps1
@@ -0,0 +1,8 @@
+Copy-Item "windows11\Square44x44Logo.targetsize-72.png" "icon-72x72.png"
+Copy-Item "windows11\Square44x44Logo.targetsize-96.png" "icon-96x96.png"
+Copy-Item "windows11\Square150x150Logo.scale-100.png" "icon-128x128.png"
+Copy-Item "windows11\Square150x150Logo.scale-100.png" "icon-144x144.png"
+Copy-Item "windows11\Square150x150Logo.scale-100.png" "icon-152x152.png"
+Copy-Item "windows11\Square150x150Logo.scale-125.png" "icon-192x192.png"
+Copy-Item "windows11\Square150x150Logo.scale-400.png" "icon-384x384.png"
+Copy-Item "windows11\LargeTile.scale-400.png" "icon-512x512.png"
